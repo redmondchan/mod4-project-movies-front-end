9985d0a1bffdc2f412c28387307a5f0c75e808b2
Redmond Chan <redmondchan92@gmail.com>
2019-03-27 10:51:53 -0400

    fixed favorites to work one time

--- src/containers/FavoritesContainer.js
+++ src/containers/FavoritesContainer.js
@@ -3,7 +3,16 @@ import MovieCard from '../components/MovieCard'
 import { Route, Switch, withRouter } from "react-router-dom";
 
 class Upcoming extends React.Component{
+  state={
+    movies: [],
+    favorites: []
+  }
+
   componentDidMount(){
+    fetch('http://localhost:3000/api/v1/movies')
+    .then(resp => resp.json())
+    .then(json => this.setState({movies: json}));
+
     let token = localStorage.token;
     fetch(`http://localhost:3000/api/v1/users/${this.props.user.id}`, {
       method: "GET",
@@ -15,18 +24,24 @@ class Upcoming extends React.Component{
     })
     .then(resp => resp.json())
     .then(json => {
-      console.log(json.user_movies)
-      console.log(json.user_movies.filter(movie => movie.favorites))
+      let allMovies = [...this.state.movies]
+      let newFavoriteMovies = []
+      let favoriteMovies = json.user_movies.filter(movie => movie.favorites)
+      allMovies.forEach((movie)=> favoriteMovies.forEach((jsonObj)=>{if (jsonObj.movie_id == movie.id){newFavoriteMovies.push(movie)}}))
+      let finalMovies = [...new Set(newFavoriteMovies)]
+      this.setState({favorites: finalMovies})
     })
     }
 
   render(){
-    // let moviesArr = this.props.movies.map(movie => <MovieCard movie={movie} addFavorites={this.props.addFavorites}/> )
+    console.log(this.state)
+    let moviesArr = this.state.favorites.map(movie => <MovieCard key={movie.id} movie={movie} addFavorites={this.props.addFavorites} user={this.props.user}/> )
     return(
       <div>
       {Object.keys(this.props.user).length > 0 ? (
         <div className="row">
           <h1>Favorites</h1>
+          {moviesArr}
           </div>
       ) : this.props.history.push("/")
     }
